70e0c675d5f4274229d92b73494bbce4b508ad44
Koki NAKAMURA <koki.nakamura22@gmail.com>
2022-09-23 21:15:05 +0900

    add the reading .env example code

--- example-app/pages/api/hello.ts
+++ example-app/pages/api/hello.ts
@@ -1,13 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { assertIsDefined } from "../helpers/assert";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const ELASTICSEARCH_URL: string | undefined = process.env.ELASTICSEARCH_URL;
+
+  assertIsDefined(ELASTICSEARCH_URL);
+
+  // res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: ELASTICSEARCH_URL });
 }
